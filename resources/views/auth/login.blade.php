e25c656e81c0d2ec06280d131192f08d267740c8
Angelo-12 <angel23.aj32@gmail.com>
2020-03-13 13:28:21 -0600

    mensajes de login y cambio de idioma

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,67 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/animsition.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/hamburgers.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/icon-font.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/daterangepicker.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/util.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-    <title>Bienvenido</title>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <title>Login</title>
 </head>
-<body>
-    <div class="limiter">
-		<div class="container-login100" style="background-image: url('img/2.png');">
-			<div class="wrap-login100 p-t-30 p-b-50">
-				<span class="login100-form-title p-b-41">
-					Iniciar Sesion
-				</span>
-				<form class="login100-form validate-form p-b-33 p-t-5">
+<body >
+    <div class="wrapper fadeInDown" >
+        <div id="formContent">
+          <!-- Tabs Titles -->
+      
+          <!-- Icon -->
+          <div class="fadeIn first">
+            <img src="img/user.png" id="icon" alt="User Icon" />
+          </div>
+      
+          <!-- Login Form -->
+          <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="User name">
-						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
-					</div>
+                @if($errors->any())
+                  <div class="alert alert-danger">
+                    @foreach ($errors->all() as $e)
+                        <li>{{$e}}</li>
+                    @endforeach
+                  </div>
+                @endif
+             
+            <div class="form-group row">
+              <div class="col-md-12">
+                <input type="email" id="email" class="fadeIn second"  name="email" placeholder="Email" required autofocus>
+               
+              </div>
+            </div>
 
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
-						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
-					</div>
-
-					<div class="container-login100-form-btn m-t-32">
-						<button class="login100-form-btn">
-							Login
-						</button>
-					</div>
-
-				</form>
-			</div>
-		</div>
-	</div>
-	
-
-	<div id="dropDownSelect1"></div>
-	
-<!--===============================================================================================-->
+            <div class="form-group row mb-0">
+              <div class="col-md-12">
+                <input type="password" id="password" class="fadeIn third" name="password" placeholder="Password" required>
+              </div>
+            </div>
+            
+                <input type="submit" class="fadeIn fourth" value="Log In">
+         
+          </form>
+      
+          <!-- Remind Passowrd -->
+          <div id="formFooter">
+             @if (Route::has('password.request'))
+                <a class="underlineHover" href="{{ route('password.request') }}">¿Olvidaste tu contraseña?</a>
+             @endif
+          </div>
+      
+        </div>
+      </div>
     <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
 
-<!--===============================================================================================-->
-    <script src="{{asset('js/animsition.min.js')}}"></script>
-<!--===============================================================================================-->
-    <script src="{{asset('js/popper.js')}}"></script>
-    
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
-<!--===============================================================================================-->
-    <script src="{{asset('js/select2.min.js')}}"></script>
-<!--===============================================================================================-->
-    <script src="{{asset('js/moment.min.js')}}"></script>
-    <script src="{{asset('js/daterangepicker.js')}}"></script>
-<!--===============================================================================================-->
-    <script src="{{asset('js/countdowntime.js')}}"></script>
-    <script src="{{asset('js/main.js')}}"></script>
-<!--===============================================================================================-->
 </body>
 </html>
